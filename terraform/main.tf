05090f66f00ebe465f26d6a653aef179294f3c26
HeyBlue24 <159277800+HeyBlue24@users.noreply.github.com>
2025-01-11 14:23:02 +0530

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,96 +1,84 @@
-provider "azurerm" {
-  features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-}
-
 # Resource Group
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-# Azure Kubernetes Service (AKS) Cluster
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_group_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.resource_group_name}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.resource_group_name}-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = var.vm_size
+  admin_username      = var.vm_admin_username
+  admin_password      = var.vm_admin_password
+
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "DevOps"
+  }
+}
+
+# Network Interface for VM
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "${var.resource_group_name}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "vm-nic-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  dns_prefix          = var.dns_prefix
+  name                = "${var.resource_group_name}-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.resource_group_name}-aks"
 
   default_node_pool {
     name       = "default"
-    node_count = var.node_count
-    vm_size    = var.node_vm_size
+    node_count = var.aks_node_count
+    vm_size    = var.aks_node_size
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  tags = var.tags
-}
-
-# Virtual Network (VNet)
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  address_space       = ["10.0.0.0/16"]
-}
-
-# Subnet for VM and AKS
-resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Network Interface for VM
-resource "azurerm_network_interface" "nic" {
-  name                = var.nic_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-# Virtual Machine (VM)
-resource "azurerm_virtual_machine" "vm" {
-  name                  = var.vm_name
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = var.vm_size
-
-  os_profile {
-    computer_name  = var.vm_name
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  storage_os_disk {
-    name              = "osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
+  tags = {
+    environment = "DevOps"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,93 +1,43 @@
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "Name of the resource group"
   type        = string
 }
 
 variable "location" {
-  description = "Azure location for resources"
+  description = "Azure region for deployment"
   type        = string
   default     = "East US"
-}
-
-variable "aks_name" {
-  description = "Name of the AKS cluster"
-  type        = string
-}
-
-variable "dns_prefix" {
-  description = "DNS prefix for the AKS cluster"
-  type        = string
-}
-
-variable "node_count" {
-  description = "Number of nodes in the AKS cluster"
-  type        = number
-  default     = 2
-}
-
-variable "node_vm_size" {
-  description = "VM size for AKS nodes"
-  type        = string
-  default     = "Standard_DS2_v2"
-}
-
-variable "vnet_name" {
-  description = "Name of the virtual network"
-  type        = string
-}
-
-variable "subnet_name" {
-  description = "Name of the subnet"
-  type        = string
-}
-
-variable "nic_name" {
-  description = "Name of the network interface"
-  type        = string
-}
-
-variable "vm_name" {
-  description = "Name of the virtual machine"
-  type        = string
 }
 
 variable "vm_size" {
   description = "Size of the virtual machine"
   type        = string
-  default     = "Standard_B1s"
+  default     = "Standard_B2s"
 }
 
-variable "admin_username" {
-  description = "Admin username for the virtual machine"
+variable "vm_admin_username" {
+  description = "Admin username for the VM"
   type        = string
 }
 
-variable "admin_password" {
-  description = "Admin password for the virtual machine"
+variable "vm_admin_password" {
+  description = "Admin password for the VM"
   type        = string
+  sensitive   = true
 }
 
-variable "tags" {
-  description = "Tags to apply to resources"
-  type        = map(string)
+variable "aks_node_count" {
+  description = "Number of worker nodes in the AKS cluster"
+  type        = number
+  default     = 1
 }
 
-variable "client_id" {
-  description = "Azure Client ID (Service Principal)"
+variable "aks_node_size" {
+  description = "Size of the worker nodes"
   type        = string
+  default     = "Standard_B2s"
 }
-
-variable "client_secret" {
-  description = "Azure Client Secret (Service Principal)"
-  type        = string
-}
-
 variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
+  description = "The ID of the Azure subscription to use"
   type        = string
 }
